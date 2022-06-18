6e79dd36ae6e22f5449e6773b650ef16d8efdf3c
VladKocherga <vlad.kocherga228@gmail.com>
2022-06-18 19:24:07 +0300

    feat: add prettier and eslint

---
+++ lets/src/components/main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+export function Main() {
+  return (
+    <>
+      <Header></Header>
+      <div className="div"></div>
+    </>
+  );
+}
+
+export default Main;
